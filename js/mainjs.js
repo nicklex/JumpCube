79da006cdf53dd263cfca55d11578cd90f315159
nicklex <nklexy@gmail.com>
2024-06-11 21:15:55 +0300

    JumpCube

--- js/mainjs.js
+++ js/mainjs.js
@@ -2,23 +2,43 @@ const jumpCountElement = document.getElementById("jumpCount");
 let jumpCount = 0;
 const cube = document.querySelector('.cube');
 const button = document.querySelector('.button');
+const buttonJumps = document.querySelector('.buttonjump');
+const buttonbuyclick = document.querySelector('.buttonbuy');
 const jumpCostElement = document.getElementById("jumpCost"); // Добавили элемент для стоимости
-let jumpHeight = 1; // Начальная высота прыжка
-let jumpCost = 10; // Начальное количество прыжков, которое тратится
+const jumpCostClickElement = document.getElementById("jumpCostclicktext");
+let jumpHeight = 0; // Начальная высота прыжка
+let clicktap = 1;
+let jumpCost = 1000;
+let jumpCostclick = 10; // Начальное количество прыжков, которое тратится
+
 
 // Функция, которая увеличивает счетчик прыжков
 function incrementJumpCount() {
   jumpCount += jumpHeight;
   jumpCountElement.textContent = jumpCount;
 }
-
+buttonJumps.addEventListener('click', ()=>{
+  jumpCount += clicktap;
+  jumpCountElement.textContent = jumpCount;
+})
+buttonbuyclick.addEventListener('click', ()=>{
+  if (jumpCount >= jumpCostclick) {
+    jumpCount -= jumpCostclick;
+    jumpCostClickElement.textContent = jumpCostclick;
+    clicktap += 1; // Увеличиваем высоту прыжка
+    jumpCostclick += 15; // Увеличиваем стоимость прыжка
+    jumpCostClickElement.textContent = jumpCostclick; // Обновляем текст стоимости 
+  } else {
+    alert('Недостаточно прыжков!');
+  }
+})
 // Функция для высоко прыжка
 function highJump() {
   if (jumpCount >= jumpCost) {
     jumpCount -= jumpCost;
     jumpCountElement.textContent = jumpCount;
     jumpHeight += 1; // Увеличиваем высоту прыжка
-    jumpCost += 5; // Увеличиваем стоимость прыжка
+    jumpCost += 500; // Увеличиваем стоимость прыжка
     jumpCostElement.textContent = jumpCost; // Обновляем текст стоимости
     cube.style.animation = `highJump ${jumpHeight}px 1s infinite alternate`; 
   } else {
@@ -26,9 +46,9 @@ function highJump() {
   }
 }
 
-// Добавляем анимацию jump в JavaScript
-cube.style.animation = 'jump 1s infinite alternate'; 
 
+// Добавляем анимацию jump в JavaScript
+cube.style.animation = 'jump 1s infinite alternate';
 // Возвращаем обработчик события для подсчета прыжков
 cube.addEventListener('animationiteration', incrementJumpCount); 
 
